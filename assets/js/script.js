f38c8f57908ef98723e792edaf3c60154ccfb38b
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-07 10:55:41 -0500

    outlined pseudo code steps for completion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// TO DO 05
+// finish 05 readme
+// take a screenshot/video
+// comment functionality
+// clear out 05 unused files
+
+// TO DO 06
+// three api calls:
+// correct weather info ; UV index ;  forecast
+
+// once you make API call, you'll get latituted and longitude info. 
+// UV API requires correct weather info
+
+// separate API call for forecast
+
+// use city information as an argument for API call functions function with argument. pass to API call. query parameter
+
+// --- after API calls are successful --- 
+// achieve display for wind speed, temperature, UV index, humidity
+// use ICON Url for displaying ICONS for weather
+
+
 var bigEl = document.createElement('h1');
 bigEl.textContent = "Hello world";
 document.body.appendChild(bigEl);
