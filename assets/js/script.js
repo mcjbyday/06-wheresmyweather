662aa9e648cbdb8509b41302240a595ea8b19f9e
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-12 16:29:41 -0500

    working functional app with acceptance criteria met

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,35 +28,35 @@ var userCityInputEl = $('#city_input');
 var searchButton = $('.btn');
 
 // event listeners for button click or search
-searchButton.on("click", weatherSearch);    
+searchButton.on("click", function () {
+        weatherSearch(userCityInputEl.val());
+    });    
 
 userCityInputEl.keydown( e => {
     if (e.keyCode === 13) {
         e.preventDefault();
-        weatherSearch();
+        weatherSearch(userCityInputEl.val());
     } 
 });    
-
 
 
 // LOCAL STORAGE
 // get local user storage data in order to append and/or render searches
 var userSearchHistory = JSON.parse(localStorage.getItem("userSearchHistory"));
 
-// if (userSearchHistory === null) {
-//     Storage.clear();
-// }
-// else if (userSearchHistory[0].searchedCityName !== null) {
-//     storeCitySearch(userSearchHistory[0].searchedCityName);
-//     getWeather(userSearchHistory[0].searchedCityName);
-//     getForecast(userSearchHistory[0].searchedCityName);
-// }
-
-
-
 // list to which past searches will be added
 var searchListContainerEl = $(".list-group");
 initializeSearchHistory();
+
+// add event listener to the 
+searchListContainerEl.click(function(e) {
+    // use event delegation to ensure clean event matches the link list items 
+    if (e.target.matches('.past-search-item')){
+        userCityInputEl.val(e.target.dataset.ref);
+        weatherSearch(e.target.dataset.ref);
+    }
+});
+
 
 // check local storage to see if there's some data to grab and display
 function initializeSearchHistory() {
@@ -66,11 +66,11 @@ function initializeSearchHistory() {
         displaySearchHistory();
     }
     else {
-        var placeholderSearchItemEl = $(`<a href="#" class="list-group-item list-group-item-action list-group-item-dark text-center" >Prior city searches will appear here...</a>`);
+        var placeholderSearchItemEl = $(`<li class="list-group-item list-group-item-action list-group-item-dark text-center past-search-item" >Prior city searches will appear here...</li>`);
         searchListContainerEl.append(placeholderSearchItemEl);
     }
 }
-    
+   
 
 // inject / display the score list into HTML
 function displaySearchHistory() {
@@ -90,7 +90,7 @@ function displaySearchHistory() {
         var city = userSearchHistory[i].searchedCityName;
         // for that list item make the list item in the DOM
 
-        var pastSearchItemEl = $(`<a href="#" class="list-group-item list-group-item-action list-group-item-dark text-center col-3" >${city}</a>`);
+        var pastSearchItemEl = $(`<li class="list-group-item list-group-item-action list-group-item-dark text-center col-3 past-search-item" data-ref="${city}" >${city}</li>`);
         // populate its text as array value
         searchListContainerEl.append(pastSearchItemEl);
     }
@@ -111,18 +111,18 @@ function storeCitySearch(searchedCity) {
 }
 
 
-function weatherSearch() {
+function weatherSearch(inputText) {
     // console.log(userCityInputEl.val())
-    var inputText = userCityInputEl.val();
-    // console.log(inputText);
     storeCitySearch(inputText);
     clearCards();
     getWeather(inputText);
     getForecast(inputText);
+    initializeSearchHistory();
 }
 
 function clearCards() {
     $(".five-day").remove();
+    $(".past-search-item").remove();
 }
 
 // RETRIEVE GEOLOCATION DATA AND CITY FORECAST DATA
@@ -180,7 +180,6 @@ function getForecast(cityname) {
     });
 }
 
-
 function uvSpanDisplay(uvi) {
     if (uvi < 3) {
         var colorFrame = "green";
@@ -197,37 +196,10 @@ function uvSpanDisplay(uvi) {
     return colorFrame;
 }
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city 
-// AND THEN that city is added to the search history
 
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-
-//  view last 10 cities searched .. i.e store inputs into local storage?
-// array of strings in local storage .. keep track of the last 10 only
-
-// TO DO 06
-// three api calls:
-// correct weather info ; UV index ;  forecast
-
-// once you make API call, you'll get latitude and longitude info. 
-// UV API requires correct weather info
-
-// separate API call for forecast
-
-// use city information as an argument for API call functions function with argument. pass to API call. use as query parameter
-
-// --- after API calls are successful --- 
-// achieve display for wind speed, temperature, UV index, humidity
-// use ICON Url for displaying ICONS for weather
